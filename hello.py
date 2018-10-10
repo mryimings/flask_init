824ca78d996d34e1e2473385eb55cfce3c23ae8c
sym19940907 <sym19940907@users.noreply.github.com>
2018-10-10 15:40:20 -0400

    POST method

--- hello.py
+++ hello.py
@@ -4,11 +4,12 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.values:
+    if request.method == "POST":
         return "username is " + request.values["username"]
-    return '<form method="get" action="/login"><input type="text" name="username" /><p><button type="submit">Submit</button></form>'
+    else:
+        return '<form method="post" action="/login"><input type="text" name="username" /><p><button type="submit">Submit</button></form>'
 
 if __name__ == '__main__':
     # set up IP address and port
