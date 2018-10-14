b26d322c59a8b6d52254c0a6f2af67b7d932b917
sym19940907 <sym19940907@users.noreply.github.com>
2018-10-14 19:51:02 -0400

    lec43 intro to cookies

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash
+from flask import Flask, request, render_template, redirect, url_for, flash, make_response
 
 app = Flask(__name__)
 
@@ -8,14 +8,26 @@ def login():
     if request.method == 'POST':
         if valid_login(request.form['username'], request.form['password']):
             flash("Successfully logged in")
-            return redirect(url_for('welcome', username=request.form.get('username')))
+            response = make_response(redirect(url_for('welcome')))
+            response.set_cookie('username', request.form.get('username'))
+            return response
         else:
             error = 'Incorrect username and password'
     return render_template('login.html', error=error)
 
-@app.route('/welcome/<username>')
-def welcome(username):
-    return render_template('welcome.html', username=username)
+@app.route('/logout')
+def logout():
+    response = make_response(redirect(url_for('login')))
+    response.set_cookie('username', '', expires=0)
+    return response
+
+@app.route('/')
+def welcome():
+    username = request.cookies.get('username')
+    if username:
+        return render_template('welcome.html', username=username)
+    else:
+        return redirect(url_for('login'))
 
 def valid_login(username, password):
     if username == password:
