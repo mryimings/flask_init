faa405425e45d0fd216fe74644fa4285c7844ebb
sym19940907 <sym19940907@users.noreply.github.com>
2018-10-15 15:17:24 -0400

    lec46 logging

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, render_template, redirect, url_for, flash, make_response
+from flask import Flask, request, render_template, redirect, url_for, flash, session
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
@@ -8,24 +10,23 @@ def login():
     if request.method == 'POST':
         if valid_login(request.form['username'], request.form['password']):
             flash("Successfully logged in")
-            response = make_response(redirect(url_for('welcome')))
-            response.set_cookie('username', request.form.get('username'))
-            return response
+            session['username'] = request.form.get('username')
+            return redirect(url_for('welcome'))
         else:
             error = 'Incorrect username and password'
+            app.logger.warning('Incorrect Username and password fot user (%s)', request.form.get('username'))
     return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
-    response = make_response(redirect(url_for('login')))
-    response.set_cookie('username', '', expires=0)
-    return response
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
 
 @app.route('/')
 def welcome():
-    username = request.cookies.get('username')
-    if username:
-        return render_template('welcome.html', username=username)
+    if 'username' in session:
+        return render_template('welcome.html', username=session['username'])
     else:
         return redirect(url_for('login'))
 
@@ -37,5 +38,9 @@ def valid_login(username, password):
 
 if __name__ == '__main__':
     app.debug = True
-    app.secret_key = 'SuperSecretKey'
+    app.secret_key = '\xe3-\xe1\xf7\xfb\x91\xb1\x8c\xae\xf2\xc1BH\xe0/K~~%>ac\t\x01'
+    #logging
+    handler = RotatingFileHandler('error.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
     app.run()
